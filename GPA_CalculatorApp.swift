d269d01899933ed71cd7117055eec1f1a9edaa48
Bon Teerapat <52702046+bonteerapat@users.noreply.github.com>
2023-05-03 17:00:11 +0700

    Add files via upload

---
+++ GPA_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPA_CalculatorApp.swift
+//  GPA_Calculator
+//
+//  Created by Bon Teerapat on 2/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct GPA_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
